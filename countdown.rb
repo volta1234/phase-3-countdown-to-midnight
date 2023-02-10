11569dcad569590e3c72e95c3b99fb726a0ff0f4
CALVINCE <calvince.ogwel@gmail.com>
2023-02-10 16:25:24 +0300

    completed bonus 2

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+def countdown(seconds)
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(seconds)
+    5.downto(1) do |i|
+        puts i
+        sleep 1
+      end
 end
